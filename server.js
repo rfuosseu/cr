9607f4f2cbebcf93c8a3fe0e2e4d81201ce74201
Rodrigue FUOSSEU <rfuosseu@MacBook-Pro-de-Rodrigue.local>
2021-09-23 10:51:15 +0200

    feature - deploy on heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+function requireHTTPS(req, res, next) {
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+
+const app = express();
+app.use(requireHTTPS);
+app.use(express.static('./dist/ui/'));
+app.use((req, res) => res.sendFile('index.html', { root: './dist/ui/' }));
+
+app.listen(process.env.PORT || 8080);

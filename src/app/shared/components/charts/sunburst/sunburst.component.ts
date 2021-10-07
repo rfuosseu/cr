dc3ca51802f2bc9f36900a886f8c068e7637e5e6
Rodrigue FUOSSEU <rfuosseu@MacBook-Pro-de-Rodrigue.local>
2021-10-07 21:20:07 +0200

    percentage - wip

--- src/app/shared/components/charts/sunburst/sunburst.component.ts
+++ src/app/shared/components/charts/sunburst/sunburst.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { throwError } from 'rxjs';
 import Sunburst from 'sunburst-chart';
 
@@ -7,26 +7,55 @@ import Sunburst from 'sunburst-chart';
   templateUrl: './sunburst.component.html',
   styleUrls: ['./sunburst.component.less']
 })
-export class SunburstComponent implements OnInit {
+export class SunburstComponent implements AfterViewInit {
   @Input() data: INode = {};
   @Input() colorLabel: string = 'color';
   @Input() size: number = 500;
+  @ViewChild('sunburst') elRef: ElementRef | undefined;
 
-  hostElement: HTMLElement;
+  currentNode: INode | undefined;
+  private _totalSize: number = 0;
 
 
-  constructor(private elRef: ElementRef) {
-    this.hostElement = this.elRef.nativeElement;
+  constructor() {
   }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this._totalSize = this._calculateValue(this.data);
+
+
     Sunburst()
       .data(this.data)
       .width(this.size)
       .height(this.size)
       .color(this.colorLabel)
+      .onHover((data) => this._displayLabel(data))
       .radiusScaleExponent(1)
-      (this.hostElement);
+      (this.elRef!.nativeElement);
+  }
+
+  getPercentage(data: any) {
+    return (data.__dataNode.value * 100 / this._totalSize).toPrecision(3);
+  }
+
+  private _displayLabel(data: any){
+    if(data) {
+      console.log(data)
+      this.currentNode = data;
+    }
+  }
+
+
+  private _calculateValue(data: INode) {
+    if(data.children) {
+      let value: number = 0;
+      data.children.forEach(ch => {
+        value += this._calculateValue(ch);
+      });
+      return value;
+    } else {
+      return data.value!;
+    }
   }
 
 }
